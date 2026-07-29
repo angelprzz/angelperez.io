8d717f9e324decf4e57eeab5ebe1d9b7677a94af
Ángel Pérez <angelpxrz@gmail.com>
2026-07-29 11:15:08 +0800

    Feat: Build hero section v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-display text-4xl font-bold">Title</h1>
-      <p className="font-sans">Body</p>
+    <div className="mx-auto w-180">
+      <h1 className="font-display text-[40px] font-extrabold text-neutral-700">Hey, I&apos;m Ángel :)</h1>
+      <h1 className="font-display text-[40px] font-semibold text-neutral-500">Software Engineer, Designer, Human</h1>
+      <div className="mt-5 flex gap-5">
+        <div className="space-y-4 font-sans text-base text-neutral-700">
+          <p>I love building digital products.</p>
+          <p>
+            I&apos;m a full-stack design engineer with 7 years of experience working across small startups and large
+            companies.
+          </p>
+          <p>My goal is to build human-centered products that help people connect, communicate and live better.</p>
+          <p>Outside of work I enjoy music, cinema and philosophy.</p>
+          <p>
+            If you&apos;re a founder or startup seeking innovative product development, let&apos;s talk. Reach out via
+            email.
+          </p>
+        </div>
+        <div className="h-53.5 w-45 shrink-0 bg-neutral-200" />
+      </div>
     </div>
   );
 }
