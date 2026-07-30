3a40a9eb2c62d9a6d522613d849367749f8b3ce3
Ángel Pérez <angelpxrz@gmail.com>
2026-07-30 21:27:07 +0800

    Refactor: Refactor page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,93 +10,101 @@ import { technologies } from "@/data/technologies";
 
 export default function Home() {
   return (
-    <div id="about" className="mx-auto w-180">
+    <div className="mx-auto w-180 space-y-8">
       <FloatingHeader />
 
-      {/* HERO */}
-      <header className="mb-8 flex h-16 items-center justify-between">
+      {/* HEADER */}
+      <header id="about" className="flex h-16 items-center justify-between">
         <HeaderLinks />
         <HeaderActions />
       </header>
 
       {/* HERO */}
-      <h1 className="font-display text-[40px] font-extrabold">Hey, I&apos;m Ángel :)</h1>
-      <h1 className="font-display text-secondary text-[40px] font-semibold">Software Engineer, Designer, Human</h1>
-
-      <div className="mt-5 flex gap-5">
-        <div className="space-y-4 font-sans text-base">
-          <p>I love building digital products.</p>
-          <p>
-            I&apos;m a full-stack design engineer with 7 years of experience working across small startups and large
-            companies.
-          </p>
-          <p>My goal is to build human-centered products that help people connect, communicate and live better.</p>
-          <p>Outside of work I enjoy music, cinema and philosophy.</p>
-          <p>
-            If you&apos;re a founder or startup seeking innovative product development, let&apos;s talk.{" "}
-            <a
-              href="mailto:angelpxrz@gmail.com"
-              className="hover:text-foreground hover:decoration-foreground text-secondary decoration-secondary cursor-pointer italic underline underline-offset-3 transition-colors"
-            >
-              Reach out via email
-            </a>
-            .
-          </p>
+      <div className="space-y-4">
+        <div className="-space-y-2">
+          <h1 className="font-display text-[40px] font-extrabold">Hey, I&apos;m Ángel :)</h1>
+          <h1 className="font-display text-secondary text-[40px] font-semibold">Software Engineer, Designer, Human</h1>
         </div>
-        <div className="bg-card h-53.5 w-45 shrink-0" />
+
+        <div className="flex gap-5">
+          <div className="space-y-4 font-sans text-base">
+            <p>I love building digital products.</p>
+            <p>
+              I&apos;m a full-stack design engineer with 7 years of experience working across small startups and large
+              companies.
+            </p>
+            <p>My goal is to build human-centered products that help people connect, communicate and live better.</p>
+            <p>Outside of work I enjoy music, cinema and philosophy.</p>
+            <p>
+              If you&apos;re a founder or startup seeking innovative product development, let&apos;s talk.{" "}
+              <a
+                href="mailto:angelpxrz@gmail.com"
+                className="hover:text-foreground hover:decoration-foreground text-secondary decoration-secondary cursor-pointer italic underline underline-offset-3 transition-colors"
+              >
+                Reach out via email
+              </a>
+              .
+            </p>
+          </div>
+          <div className="bg-card h-53.5 w-45 shrink-0" />
+        </div>
       </div>
 
       {/* PROJECTS */}
-      <h2 id="projects" className="font-display text-foreground mt-8 mb-4 text-[40px] font-extrabold">
-        Projects
-      </h2>
-
-      {/* Cards */}
-      <div className="space-y-8">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
+      <div className="space-y-4">
+        <h2 id="projects" className="font-display text-foreground text-[40px] font-extrabold">
+          Projects
+        </h2>
+        <div className="space-y-8">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} {...project} />
+          ))}
+        </div>
       </div>
 
       {/* TECHNOLOGIES */}
-      <h2 className="font-display text-foreground mt-8 mb-4 text-[40px] font-extrabold">Technologies</h2>
-      <div className="flex flex-wrap gap-x-2 gap-y-4">
-        {technologies.map((tech) => (
-          <TechPill key={tech.text} {...tech} />
-        ))}
+      <div className="space-y-4">
+        <h2 className="font-display text-foreground text-[40px] font-extrabold">Technologies</h2>
+        <div className="flex flex-wrap gap-x-2 gap-y-4">
+          {technologies.map((tech) => (
+            <TechPill key={tech.text} {...tech} />
+          ))}
+        </div>
       </div>
 
       {/* CONTACT */}
-      <h2 id="contact" className="font-display text-foreground mt-8 mb-4 text-[40px] font-extrabold">
-        Get in touch
-      </h2>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-1">
-          <h3 className="font-display text-lg font-medium">Email</h3>
-          <ContactButton {...email} />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <h3 className="font-display text-lg font-medium">Social Media</h3>
-          <div className="flex gap-4">
-            {socials.map((social) => (
-              <ContactButton key={social.name} {...social} />
-            ))}
+      <div className="space-y-4">
+        <h2 id="contact" className="font-display text-foreground text-[40px] font-extrabold">
+          Get in touch
+        </h2>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-1">
+            <h3 className="font-display text-lg font-medium">Email</h3>
+            <ContactButton {...email} />
           </div>
-        </div>
 
-        <div className="flex flex-col gap-1">
-          <h3 className="font-display text-lg font-medium">Other</h3>
-          <div className="flex gap-4">
-            {others.map((item) => (
-              <ContactButton key={item.name} {...item} />
-            ))}
+          <div className="flex flex-col gap-1">
+            <h3 className="font-display text-lg font-medium">Social Media</h3>
+            <div className="flex gap-4">
+              {socials.map((social) => (
+                <ContactButton key={social.name} {...social} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <h3 className="font-display text-lg font-medium">Other</h3>
+            <div className="flex gap-4">
+              {others.map((item) => (
+                <ContactButton key={item.name} {...item} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
 
       {/* FOOTER */}
-      <div className="bg-card mt-8 h-px w-full" />
+      <div className="bg-card h-px w-full" />
       <div className="my-8 flex flex-col items-center gap-2">
         <p className="text-center font-sans text-base font-normal">© 2026 Ángel Pérez • Handcrafted with ❤️️</p>
         <p className="text-muted text-center font-sans text-xs font-light">
