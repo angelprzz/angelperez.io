f9935550b99a944dee40cf587fed0b6a6039c4e8
Ángel Pérez <angelpxrz@gmail.com>
2026-07-30 15:43:52 +0800

    Chore: Update styles of contact-button

--- src/components/contact-button.tsx
+++ src/components/contact-button.tsx
@@ -7,10 +7,10 @@ export default function ContactButton({ icon: Icon, name, rightIcon: RightIcon, 
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex w-fit cursor-pointer items-center gap-2 rounded-lg border border-neutral-200 bg-[#f7f7f7] px-6 py-5 shadow-xs transition-transform duration-300 hover:scale-102"
+      className="flex w-fit cursor-pointer items-center gap-2 rounded-lg border border-neutral-200 bg-[#f7f7f7] px-6 py-5 transition-all duration-300 hover:scale-102 hover:shadow-xs"
     >
       <Icon size={24} />
-      <span className="text-foreground font-sans text-lg font-medium underline">{name}</span>
+      <span className="text-foreground font-sans text-lg font-medium underline underline-offset-3">{name}</span>
       {RightIcon && <RightIcon size={20} className="ml-2" />}
     </Link>
   );
