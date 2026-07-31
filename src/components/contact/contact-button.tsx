2b9b6ed63969c2946743d349269767aa6222f43c
Ángel Pérez <angelpxrz@gmail.com>
2026-07-31 18:17:58 +0800

    Fix: Update dimensions of contact-button

--- src/components/contact/contact-button.tsx
+++ src/components/contact/contact-button.tsx
@@ -7,7 +7,7 @@ export default function ContactButton({ icon: Icon, iconStyle, name, rightIcon: 
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="border-border bg-card flex w-fit cursor-pointer items-center gap-2 rounded-lg border px-6 py-5 transition-all duration-300 hover:scale-102 hover:shadow-xs"
+      className="border-border bg-card flex w-fit cursor-pointer items-center gap-2 rounded-lg border px-5 py-4 transition-all duration-300 hover:scale-102 hover:shadow-xs sm:px-6 sm:py-5"
     >
       <Icon size={24} className={iconStyle} />
       <span className="text-foreground font-sans text-sm font-medium sm:text-lg underline underline-offset-3">{name}</span>
